ef8b8e3c9522574032364437adc44be42e07ff9b
chris <58859629+christophschaller@users.noreply.github.com>
2025-04-24 23:17:04 +0200

    refactor: more typing for the IconWBackground layer

--- src/components/react/IconWithBackgroundLayer.ts
+++ src/components/react/IconWithBackgroundLayer.ts
@@ -1,23 +1,37 @@
+import type { PointOfInterest } from "@/types";
 import { CompositeLayer } from "@deck.gl/core";
+import type { CompositeLayerProps } from "@deck.gl/core";
 import { ScatterplotLayer, IconLayer } from "@deck.gl/layers";
 
-export default class IconWithBackgroundLayer extends CompositeLayer {
+interface IconWithBackgroundProps extends CompositeLayerProps {
+  data: any[];
+  getPosition?: (d: any) => [number, number];
+  getIcon?: (d: any) => any;
+  getSize?: (d: any) => number;
+  getColor?: (d: any) => [number, number, number, number];
+  getBackgroundRadius?: (d: any) => number;
+  getBackgroundColor?: (d: any) => [number, number, number, number];
+  getFilterValue?: (d: any) => any;
+  filterRange?: (d: any) => any;
+}
+
+export default class IconWithBackgroundLayer extends CompositeLayer<IconWithBackgroundProps> {
   static layerName = "IconWithBackgroundLayer";
 
   static defaultProps = {
     // === Shared Base Props (inherited by both sublayers) ===
-    getPosition: { type: "accessor", value: (x) => x.position },
+    getPosition: { type: "accessor", value: (x: any) => x.position },
     pickable: { type: "boolean", value: true },
     visible: { type: "boolean", value: true },
 
     // === IconLayer Props ===
-    getIcon: { type: "accessor", value: (d) => d.icon }, // expects {url, width, height, mask?}
+    getIcon: { type: "accessor", value: (d: PointOfInterest) => d.icon }, // expects {url, width, height, mask?}
     getSize: { type: "accessor", value: 24 },
     getColor: { type: "accessor", value: [255, 255, 255] },
 
     // === DataFilterExtension Props (if used) ===
-    getFilterValue: { type: "accessor", value: (x) => x.filterValue },
-    filterRange: { type: "array", value: [0, 1] },
+    getFilterValue: { type: "accessor", value: (x: any) => x.filterValue },
+    filterRange: { type: "array", value: [0, 1] as [number, number] },
 
     // === Background Circle Props ===
     getBackgroundRadius: { type: "accessor", value: 16 },
