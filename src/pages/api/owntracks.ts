f72759a86a763a62d7a28db635f015765809fa50
chris <58859629+christophschaller@users.noreply.github.com>
2025-04-27 13:48:15 +0200

    feat: add server route to query owntracks

---
+++ src/pages/api/owntracks.ts
@@ -0,0 +1,54 @@
+import type { APIRoute } from "astro";
+import fs from "fs/promises";
+import path from "path";
+
+let riderData: any = null;
+
+async function loadRiderData() {
+  if (riderData === null) {
+    const dataPath = path.resolve("./data/shardana_riders.json");
+    const fileContent = await fs.readFile(dataPath, "utf-8");
+    riderData = JSON.parse(fileContent);
+  }
+  return riderData;
+}
+
+export const GET: APIRoute = async () => {
+  const username = import.meta.env.OWNTRACKS_USERNAME;
+  const password = import.meta.env.OWNTRACKS_PASSWORD;
+  const auth = Buffer.from(`${username}:${password}`).toString("base64");
+
+  const riderData = loadRiderData();
+
+  const response = await fetch(
+    "https://tracking.roadbook.bike/owntracks/api/0/last",
+    {
+      headers: {
+        Authorization: `Basic ${auth}`,
+      },
+    },
+  );
+
+  if (!response.ok) {
+    return new Response(JSON.stringify({ error: "Failed to fetch data" }), {
+      status: 500,
+    });
+  }
+
+  const data = await response.json();
+  const riders = await riderData;
+  data.forEach((item: any) => {
+    const rider = riders[item.username];
+    if (rider) {
+      item.display_name = rider.display_name;
+      item.cap_number = rider.cap_number;
+    }
+  });
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
