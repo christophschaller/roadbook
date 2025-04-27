050535822d5b729f13d49be7cab9795d7aba9c1e
chris <58859629+christophschaller@users.noreply.github.com>
2025-04-27 13:48:39 +0200

    feat: add fetcher store for rider information from server

---
+++ src/stores/index.ts
@@ -0,0 +1,9 @@
+export { areaStore } from "./areaStore";
+export { poiStore } from "./poiStore";
+export {
+  resourceStore,
+  resourceStateStore,
+  resourceViewStore,
+} from "./resourceStore";
+export { riderStore } from "./riderStore";
+export { trackStore } from "./trackStore";
---
+++ src/stores/riderStore.ts
@@ -0,0 +1,14 @@
+import { nanoquery } from "@nanostores/query";
+import type { Rider } from "@/types";
+
+export const [createFetcherStore] = nanoquery({
+  fetcher: (...keys) => fetch(keys.join("")).then((r) => r.json()),
+});
+
+export const riderStore = createFetcherStore<Rider>(
+  "/api/owntracks",
+  {
+    revalidateOnFocus: true,
+    revalidateOnReconnect: true,
+    revalidateInterval: 30,
+  });
