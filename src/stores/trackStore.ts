b462f245ef7ab8003f8b7daaa5a7450b30954218
chris <58859629+christophschaller@users.noreply.github.com>
2025-04-29 19:08:17 +0200

    feat: make selectedTrack persistent

--- src/stores/trackStore.ts
+++ src/stores/trackStore.ts
@@ -1,4 +1,4 @@
-import { atom } from "nanostores";
+import { persistentAtom } from "@nanostores/persistent";
 import type { Track } from "@/types";
 import { nanoquery } from "@nanostores/query";
 
@@ -6,7 +6,10 @@ export const [createFetcherStore] = nanoquery({
   fetcher: (...keys) => fetch(keys.join("")).then((res) => res.json()),
 });
 
-export const $selectedTrack = atom<string>("shardana_full.json");
+export const $selectedTrack = persistentAtom<string>(
+  "selectedTrack",
+  "shardana_full.json",
+);
 
 export const $trackStore = createFetcherStore<Track>([
   "",
