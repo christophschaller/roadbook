d10f8ad1c8c4b7c068bbd4877bcaf4e140842492
chris <58859629+christophschaller@users.noreply.github.com>
2025-04-29 19:07:02 +0200

    feat: make displayRiders toggle persistent

--- src/stores/riderStore.ts
+++ src/stores/riderStore.ts
@@ -1,3 +1,4 @@
+import { persistentAtom } from "@nanostores/persistent";
 import { nanoquery } from "@nanostores/query";
 import type { Rider } from "@/types";
 
@@ -5,8 +6,13 @@ export const [createFetcherStore] = nanoquery({
   fetcher: (...keys) => fetch(keys.join("")).then((r) => r.json()),
 });
 
-export const riderStore = createFetcherStore<Rider>("/api/owntracks", {
+export const $riderStore = createFetcherStore<Rider[]>("/api/owntracks", {
   revalidateOnFocus: true,
   revalidateOnReconnect: true,
   revalidateInterval: 60000,
+});
+
+export const $displayRiders = persistentAtom<boolean>("displayRiders", true, {
+  encode: JSON.stringify,
+  decode: JSON.parse,
 });
