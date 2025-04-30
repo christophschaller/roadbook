a70eea34709b94fac6409d80f43b9f181b087757
chris <58859629+christophschaller@users.noreply.github.com>
2025-04-30 21:43:29 +0200

    feat: add store to manage which rider to focus the map on

--- src/stores/riderStore.ts
+++ src/stores/riderStore.ts
@@ -1,3 +1,4 @@
+import { atom } from "nanostores";
 import { persistentAtom } from "@nanostores/persistent";
 import { nanoquery } from "@nanostores/query";
 import type { Rider } from "@/types";
@@ -16,3 +17,5 @@ export const $displayRiders = persistentAtom<boolean>("displayRiders", true, {
   encode: JSON.stringify,
   decode: JSON.parse,
 });
+
+export const $focusRider = atom<Rider | null>(null);
